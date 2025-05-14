048657d1fb54bd41b200ec6178682cf1554e03c7
Hamza <sougouhamza@gmail.com>
2025-05-14 21:11:46 +0000

    Database Config & web token

--- actions/userController.js
+++ actions/userController.js
@@ -1,5 +1,10 @@
 "use server";
 
+import { cookies } from "next/headers";
+import { getCollection } from "../lib/db";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 function isAlphanumeric(str) {
   const regex = /^[a-zA-Z0-9]*$/;
   return regex.test(str);
@@ -19,6 +24,8 @@ export const register = async function (prevState, formData) {
   ourUser.username = ourUser.username.trim();
   ourUser.password = ourUser.password.trim();
 
+  // Validation du nom d'utilisateurS
+
   if (ourUser.username.length < 3) {
     errors.username =
       "Le nom d'utilisateur doit contenir au moins 3 caractères";
@@ -28,6 +35,29 @@ export const register = async function (prevState, formData) {
       "Le nom d'utilisateur doit contenir au maximum 30 caractères";
   }
 
+  if (!isAlphanumeric(ourUser.username)) {
+    errors.username =
+      "Le nom d'utilisateur ne doit contenir que des lettres et des chiffres";
+  }
+
+  if (ourUser.username == "") {
+    errors.username = "Le nom d'utilisateur ne peut pas être vide";
+  }
+
+  // Validation du mot de passe
+
+  if (ourUser.password.length < 3) {
+    errors.password = "Le mot de passe doit contenir au moins 3 caractères";
+  }
+
+  if (ourUser.password.length > 30) {
+    errors.password = "Le mot de passe doit contenir au maximum 30 caractères";
+  }
+
+  if (ourUser.password == "") {
+    errors.username = "Le mot de passe ne peut pas être vide";
+  }
+
   if (errors.username || errors.password) {
     return {
       errors: errors,
@@ -35,10 +65,33 @@ export const register = async function (prevState, formData) {
     };
   }
 
-  if (!isAlphanumeric(ourUser.username)) {
-    errors.username =
-      "Le nom d'utilisateur ne doit contenir que des lettres et des chiffres";
-  }
+  const salt = bcrypt.genSaltSync(10);
+  ourUser.password = bcrypt.hashSync(ourUser.password, salt);
+
+  // Stockage de l'utilisateur dans la base de données
+  const usersCollection = await getCollection("users");
+  const newUser = await usersCollection.insertOne({ ourUser });
+  const userId = newUser.insertedId.toString();
+
+  // Création du token JWT
+  const token = jwt.sign(
+    {
+      id: userId,
+      username: ourUser.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  // Connecter l'utilisateur en lui donnant un cookie
+  cookies().set("ourhaikuapp", token, {
+    httpOnly: true,
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24,
+    secure: true,
+  });
 
   return {
     success: true,
